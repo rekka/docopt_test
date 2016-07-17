5e89c5bd4762d16963f2567320293b19eaea40cb
Norbert Pozar <bertapozar@gmail.com>
2016-07-17 18:59:23 +0900

    reorder fields of Args

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ extern crate rustc_serialize;
 
 #[derive(Debug, RustcDecodable)]
 pub struct Args {
-    pub arg_test: Option<String>,
     pub flag_n: Option<usize>,
+    pub arg_test: Option<String>,
 }
